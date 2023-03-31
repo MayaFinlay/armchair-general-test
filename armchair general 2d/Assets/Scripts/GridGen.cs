080eaffa99a35276b944e93a42996584c4900304
gracefulzebra <73586102+gracefulzebra@users.noreply.github.com>
2023-03-31 16:09:55 +0100

    Grid added

---
+++ armchair general 2d/Assets/Scripts/GridGen.cs
@@ -0,0 +1,110 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GridGen : MonoBehaviour
+{
+    [HideInInspector]
+    public Node[,] grid { get; private set; }
+
+    [SerializeField] private Vector2 gridWorldSize;
+    private int gridSizeX;
+    private int gridSizeY;
+    [SerializeField] private float nodeRadius;
+    private float nodeDiameter;
+
+    [SerializeField] private bool debugMode = false;
+
+    private void Awake()
+    {
+        nodeDiameter = nodeRadius + nodeRadius;
+        gridSizeX = Mathf.RoundToInt(gridWorldSize.x / nodeDiameter);
+        gridSizeY = Mathf.RoundToInt(gridWorldSize.y / nodeDiameter);
+        CreateGrid();
+    }
+
+    private void Update()
+    {
+        if (debugMode)
+        {
+            Vector3 mousePos = Input.mousePosition;
+            Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(mousePos);
+            print(GetNodeFromWorldPoint(mouseWorldPos).x + " " + GetNodeFromWorldPoint(mouseWorldPos).y);
+        }
+    }
+
+    void CreateGrid()
+    {
+        grid = new Node[gridSizeX, gridSizeY];
+        Vector3 worldBottomLeft = transform.position - Vector3.right * gridSizeX / 2 - Vector3.up * gridSizeY / 2;
+
+        for (int x = 0; x < gridSizeX; x++)
+        {
+            for (int y = 0; y < gridSizeY; y++)
+            {
+                Vector3 worldPoint = worldBottomLeft + (Vector3.right * (x * nodeDiameter + nodeRadius)) + (Vector3.up * (y * nodeDiameter + nodeRadius));
+                grid[x, y] = new Node(x, y, true, worldPoint);
+            }
+        }
+    }
+
+    public Node GetNodeFromWorldPoint(Vector3 worldPosition)
+    {
+        float percentX = (worldPosition.x + gridWorldSize.x / 2) / gridWorldSize.x;
+        float percentY = (worldPosition.y + gridWorldSize.y / 2) / gridWorldSize.y;
+        percentX = Mathf.Clamp01(percentX);
+        percentY = Mathf.Clamp01(percentY);
+
+        int x = Mathf.FloorToInt(Mathf.Clamp((gridSizeX) * percentX, 0, gridSizeX - 1));
+        int y = Mathf.FloorToInt(Mathf.Clamp((gridSizeY) * percentY, 0, gridSizeY - 1));
+
+        return grid[x, y];
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(transform.position, new Vector3(gridWorldSize.x, gridWorldSize.y, 0f));
+
+        if (grid != null && debugMode)
+        {
+            foreach (Node n in grid)
+            {
+                Gizmos.color = (n.walkable) ? Color.white : Color.red;
+                Gizmos.DrawCube(n.worldPosition, Vector3.one * (nodeDiameter - .1f));
+            }
+        }
+    }
+
+    public List<Node> GetNeighbours(Node node)
+    {
+        List<Node> neighbours = new List<Node>();
+
+        for (int x = -1; x <= 1; x++)
+        {
+            if (x == 0)
+                continue;
+
+            int checkX = node.x + x;
+
+            if (checkX >= 0 && checkX < gridSizeX)
+            {
+                neighbours.Add(grid[checkX, node.y]);
+            }
+        }
+
+        for (int y = -1; y <= 1; y++)
+        {
+            if (y == 0)
+                continue;
+
+
+            int checkY = node.y + y;
+
+            if (checkY >= 0 && checkY < gridSizeY)
+            {
+                neighbours.Add(grid[node.x, checkY]);
+            }
+        }
+
+        return neighbours;
+    }
+}
---
+++ armchair general 2d/Assets/Scripts/Node.cs
@@ -0,0 +1,42 @@
+using System;
+using UnityEngine;
+
+public class Node : IComparable<Node> 
+{
+    //Node Attributes
+    public int x, y;
+    public bool walkable;
+    public Vector2 worldPosition;
+
+    //Pathfinding
+    public Node parent;
+    public Node[] neighbours; 
+    public int gCost, hCost;
+    public int fCost
+    {
+        get
+        {
+            return gCost + hCost;
+        }
+    }
+
+    //Constructor
+    public Node(int _xCoord, int _yCoord, bool _walkable, Vector2 _worldPosition)
+    {
+        x = _xCoord;
+        y = _yCoord;
+        walkable = _walkable;
+        worldPosition = _worldPosition;
+    }
+
+    //Interface Compare Function For Pathfinding
+    public int CompareTo(Node nodeToCompare)
+    {
+        int compare = fCost.CompareTo(nodeToCompare.fCost);
+        if (compare == 0)
+        {
+            compare = hCost.CompareTo(nodeToCompare.hCost);
+        }
+        return compare;
+    }
+}
