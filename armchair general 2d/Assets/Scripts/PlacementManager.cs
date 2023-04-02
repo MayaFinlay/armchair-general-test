d7d6c68f56a040eabd07a45656f621185ada80cd
gracefulzebra <73586102+gracefulzebra@users.noreply.github.com>
2023-04-03 00:46:19 +0100

    Placement Change

--- armchair general 2d/Assets/Scripts/PlacementManager.cs
+++ armchair general 2d/Assets/Scripts/PlacementManager.cs
@@ -7,6 +7,8 @@ public class PlacementManager : MonoBehaviour
     [Header("Functionality")]
     [SerializeField] private GridGen gridReference;
     [SerializeField] private GameObject[] unitPrefabs;
+    [SerializeField] private GameObject placementIcon;
+    [SerializeField] private Sprite[] unitSprites;
     private Vector3 rawMousePos;
     private Vector3 worldMousePos;
 
@@ -15,56 +17,76 @@ public class PlacementManager : MonoBehaviour
 
     public void PlaceGrunt()
     {
-        UnitSelected();
         unitToBePlaced = 0;
+        UnitSelected();
     }
 
     public void PlaceSniper()
     {
-        UnitSelected();
         unitToBePlaced = 1;
+        UnitSelected();
     }
 
     public void PlaceTank()
     {
-        UnitSelected();
         unitToBePlaced = 2;
+        UnitSelected();
     }
 
     private void UnitSelected()
     {
+        placementIcon.GetComponent<SpriteRenderer>().sprite = unitSprites[unitToBePlaced];
+        placementIcon.SetActive(true);
         if (!unitSelected)
             unitSelected = true;
     }
 
     void Update()
     {
+        CursorPosition();
+        PlacementIndicatorFollow();
+        UnitPlacement();
+    }
+
+    private void CursorPosition()
+    {
+        rawMousePos = Input.mousePosition;
+        worldMousePos = Camera.main.ScreenToWorldPoint(rawMousePos);
+        worldMousePos.z = 0f;
+    }
+
+    private void UnitPlacement()
+    {
         if (unitSelected && Input.GetMouseButtonDown(0))
         {
             if (CursorOverGrid())
             {
-                rawMousePos = Input.mousePosition;
-                worldMousePos = Camera.main.ScreenToWorldPoint(rawMousePos);
-
                 Node targetNode = gridReference.GetNodeFromWorldPoint(worldMousePos);
-                
-                if(!targetNode.hasUnit)
+
+                if (!targetNode.hasUnit)
                 {
                     Vector3 gridSquarePos = targetNode.worldPosition;
                     targetNode.hasUnit = true;
                     Instantiate(unitPrefabs[unitToBePlaced], gridSquarePos, Quaternion.identity);
                 }
             }
+            placementIcon.SetActive(false);
+            unitSelected = false;
+        }
+        else if (Input.GetMouseButtonDown(1))
+        {
             unitSelected = false;
         }
     }
+
+    private void PlacementIndicatorFollow()
+    {
+        placementIcon.transform.position = worldMousePos;
+    }
+    
     //Checks to see if cursor is in bounds of grid
     private bool CursorOverGrid()
     {
-        rawMousePos = Input.mousePosition;
-
-        worldMousePos = Camera.main.ScreenToWorldPoint(rawMousePos);
-
         if(worldMousePos.x < gridReference.grid[gridReference.gridSizeX - 1, gridReference.gridSizeY - 1].worldPosition.x + gridReference.nodeRadius && worldMousePos.y < gridReference.grid[gridReference.gridSizeX - 1, gridReference.gridSizeY - 1].worldPosition.y + gridReference.nodeRadius)
         {
             if (worldMousePos.x > gridReference.grid[0, 0].worldPosition.x - gridReference.nodeRadius && worldMousePos.y > gridReference.grid[0, 0].worldPosition.y - gridReference.nodeRadius)
